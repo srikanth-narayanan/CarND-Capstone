a4974217c62bc897638ec299b36d7216188cd730
Srikanth Narayanan <srikanthnarayanan@Srikanths-MacBook-Pro.local>
2018-01-08 00:00:38 -0800

    Using a value from the PID control project

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -20,7 +20,7 @@ class Controller(object):
         self.yaw_control = YawController(wheel_base, steer_ratio, min_speed,
                                          max_lat_accel, max_steer_angle)
         # Initialise PID Control
-        self.pid_control = PID(kp=15, ki=1.5, kd=1.5, mn=decel_limit, mx=accel_limit)
+        self.pid_control = PID(kp=0.290158, ki=9.1e-07, kd=0.1, mn=decel_limit, mx=accel_limit)
         # Lowpass filter for Steering
         self.lowpass_steering = LowPassFilter(tau=3, ts=1)
         #Low pass filter for throttle
